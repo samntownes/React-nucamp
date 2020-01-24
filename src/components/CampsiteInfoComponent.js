ee3cb5928aa232d728ceea276ae7cab61a6f54c7
Sam Amos <sma.amos@gmail.com>
2020-01-23 23:38:41 -0500

    redux thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrum
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { render } from '@testing-library/react';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 
 const required = val => val && val.length;
@@ -159,6 +160,26 @@ function RenderComments({comments, addComment, campsiteId}) {
     };
 
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     if (props.campsite) {
         return (
             <div className="container">
